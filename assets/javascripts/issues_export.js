be942715c45e936edeb437d898ecbbacd3191d37
Jacek Grzybowski <jgrzybowski@artegence.com>
2015-05-13 16:24:03 +0200

    style fix for default theme

--- assets/javascripts/issues_export.js
+++ assets/javascripts/issues_export.js
@@ -2,7 +2,7 @@ $(function(){
   $('#issue_tree').append( $('#subtasks-export') );
 
   if ( $('#related-export').length > 0 ) {
-    $('#relations').css("margin-bottom", "18px");
+    $('#relations').css("margin-bottom", "28px");
     $('#relations table.list').after( $('#related-export') );
   }
 });
