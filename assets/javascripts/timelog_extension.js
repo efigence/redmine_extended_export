3eaadb19e7079dc1350bb8e1d5f7a5d8d72a2503
Jacek Grzybowski <jgrzybowski@artegence.com>
2015-05-20 10:49:03 +0200

    timelog hook: export links

--- assets/javascripts/timelog_extension.js
+++ assets/javascripts/timelog_extension.js
@@ -1,5 +1,11 @@
 $(function(){
-  if (document.location.pathname.match(/time_entries/)) {
-    // TODO
-  }
+  $('#csv-export-options').after( $('#xlsx-export-options') );
+
+  $('a.csv').parent().after( $('a.xlsx').parent() );
+  $('a.xlsx').parent().after( $('a.pdf').parent() );
+
+  $('p.other-formats').last().remove();
+
+  $('a.csv').css("margin-right", "3px");
+  $('a.xlsx').css("margin-right", "3px");
 });
