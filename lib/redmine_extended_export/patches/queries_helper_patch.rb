dca0ef105b49f51d74e44c6352a6e7b554bfeadd
Janek <jlachowicz@artegence.com>
2016-03-07 12:47:15 +0100

    add lp

--- lib/redmine_extended_export/patches/queries_helper_patch.rb
+++ lib/redmine_extended_export/patches/queries_helper_patch.rb
@@ -8,6 +8,7 @@ module RedmineExtendedExport
         base.send(:include, InstanceMethods)
         base.class_eval do
           unloadable
+          alias_method_chain :query_to_csv, :lp
         end
       end
 
@@ -17,23 +18,43 @@ module RedmineExtendedExport
 
           p = Axlsx::Package.new
           p.workbook.add_worksheet(:name => "issues") do |sheet|
-            columns = (options[:columns] == 'all' ? query.available_inline_columns : query.inline_columns)
+            columns = (options[:columns] == 'all' ? query.available_inline_columns : ['L.p.'] + query.inline_columns)
             query.available_block_columns.each do |column|
               if options[column.name].present?
                 columns << column
               end
             end
-            sheet.add_row columns.collect {|c| Redmine::CodesetUtil.from_utf8(c.caption.to_s, encoding) }
+            sheet.add_row columns.collect {|c| Redmine::CodesetUtil.from_utf8((c.try(:caption) || c).to_s, encoding) }
 
-            items.each do |item|
-              row = columns.collect {|c| Redmine::CodesetUtil.from_utf8(csv_content(c, item), encoding) }
-              puts row.inspect
+            items.each.with_index do |item, index|
+              row = [index + 1] + columns[1..-1].collect {|c| Redmine::CodesetUtil.from_utf8(csv_content(c, item), encoding) }
               sheet.add_row row, types: []
               sheet.add_hyperlink :location => issue_url(item.id), :ref => sheet.rows.last.cells.first
             end
           end
 
           p.to_stream.read
+        end
+
+        def query_to_csv_with_lp(items, query, options={})
+          encoding = l(:general_csv_encoding)
+          columns = (options[:columns] == 'all' ? query.available_inline_columns : ['L.p.'] + query.inline_columns)
+
+          query.available_block_columns.each do |column|
+            if options[column.name].present?
+              columns << column
+            end
+          end
+
+          export = CSV.generate(:col_sep => l(:general_csv_separator)) do |csv|
+            # csv header fields
+            csv << columns.collect {|c| Redmine::CodesetUtil.from_utf8((c.try(:caption) || c).to_s, encoding) }
+            # csv lines
+            items.each.with_index do |item, index|
+              csv << [index + 1] + columns[1..-1].collect {|c| Redmine::CodesetUtil.from_utf8(csv_content(c, item), encoding) }
+            end
+          end
+          export
         end
       end
     end
